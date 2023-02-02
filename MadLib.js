6817f5678d3efe29d6e62b7b5e3215c6f2fe2f02
ehauptman <esperanzahauptman@gmail.com>
2023-02-02 13:23:04 -0700

    adding madlib

--- MadLib.js
+++ MadLib.js
@@ -11,7 +11,6 @@ button.addEventListener("click", story);
 function story(){
     //console.log(a1.value.length);
     const story2 = `When I had no shine, I used ${a1.value} for sparkle. When my ${a2.value} home ${v.value}, I made a home out of ${n2.value}. When love was gone, I found love in ${n1.value}. When the forest was gone, I made trees out of ${a2.value} ${n3.value}.`;
-    const story1 = `A vacation is when you take a trip to some ${a1.value} place with your ${a2.value} family. Usually you go to some place that is near a/an ${n1.value} or up on a/an ${a2.value}. Sometimes, if you are lucky you get to go ${v.value} on the beach or see a ${n3.value} in a zoo.`;
     if (notEmpty()){
         document.getElementById("output").innerHTML = story2;
         document.getElementById("output").style.color = 'black';
